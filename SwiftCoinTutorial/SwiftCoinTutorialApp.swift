2e131c578e8c5fcb6f691056ac821c65e822fe49
Carlos Alberto Lira <47789867+carlosalberto05@users.noreply.github.com>
2023-04-25 20:50:46 -0600

    Initial Commit

---
+++ SwiftCoinTutorial/SwiftCoinTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinTutorialApp.swift
+//  SwiftCoinTutorial
+//
+//  Created by Carlos Alberto on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
